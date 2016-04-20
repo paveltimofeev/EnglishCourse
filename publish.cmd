76f5a45aa6da70dd6cd3f64a59144121c0a420db
Pavel Timofeev <pavel.timofeev@hotmail.com>
2016-04-20 20:09:30 +0400

    merge

---
+++ publish.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate set MyDate=%%x
+set today=%MyDate:~0,4%-%MyDate:~4,2%-%MyDate:~6,2%
+echo "publish update at %today%"
+
+git status
+set /p comment=Add comment to commit:
+echo.
+echo Commit will commented with: 
+echo "publish update at %today%: %comment%"
+echo.
+echo %today% > _includes/updated.md
+pause
+
+git add --all
+git status
+git commit -m "publish update at %today%: %comment%"
+pause
+
+git push origin gh-pages
+pause
